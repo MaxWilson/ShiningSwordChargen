4d642d818b9bfc5e9a988920bed6a5e69a4c3501
Max Wilson <wilson.max@gmail.com>
2020-06-17 20:10:34 -0700

    Add skeleton

---
+++ src/App/App.fs
@@ -0,0 +1,27 @@
+﻿module App
+
+open Elmish
+open Fable.SimpleHttp
+open Thoth.Json
+open Feliz
+
+type UI = {
+    txt: string
+    }
+type Msg =
+    | Update of transform: (UI -> UI)
+
+let init _ = { txt = "" }, Cmd.Empty
+let update msg model =
+    match msg with
+    | Update t ->
+        (t model), Cmd.Empty
+
+let view model dispatch =
+    Html.div [
+        prop.className "content"
+        prop.children [
+            Html.h1 "Hello"
+            Html.div "world"
+            ]
+        ]
---
+++ src/App/Main.fs
@@ -0,0 +1,12 @@
+module Main
+
+open Elmish
+open Elmish.React
+open Feliz
+open Fable.SimpleHttp
+open App
+open Elmish.HMR
+
+Program.mkProgram init update view
+|> Program.withReactSynchronous "elmish-app"
+|> Program.run
